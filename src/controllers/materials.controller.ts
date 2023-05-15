7ee463821f2808893dc639118062fd557751cf9a
Yabaze Cool <cool.mirakle@gmail.com>
2023-05-15 09:02:58 +0530

    Material List API created

---
+++ src/controllers/materials.controller.ts
@@ -0,0 +1,82 @@
+import {inject} from '@loopback/core';
+import {
+  Request,
+  RestBindings,
+  get,
+  param, post, getModelSchemaRef, requestBody,
+} from '@loopback/rest';
+import {authenticate} from '@loopback/authentication';
+import {ProjectRepository} from '../repositories/project.repository';
+import {MongoDatasource} from '../datasources';
+import {ProjectDetails} from '../models';
+
+/**
+ * A simple controller to bounce back http requests
+ */
+@authenticate('basic')
+export class MaterialsController {
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {
+  }
+
+  repository = new ProjectRepository();
+
+  @get('/v1/materials/{id}')
+  getProjectById(
+    @param.path.string('id') id: string,
+  ): object {
+    // Reply with a greeting, the current time, the url, and request headers
+    return new Promise<any>((resolve, reject) => {
+      MongoDatasource.getClient().then(client => {
+        if (id === 'all') {
+          resolve(this.repository.getAllMaterialDetails(client, 'Material details received successfully.'));
+        } else {
+          resolve(this.repository.getMaterialInformation(client, 'Material details received successfully.', id));
+        }
+      });
+    });
+  }
+
+  @post('/v1/project/update')
+  updateProjectInformation(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(ProjectDetails, {
+            title: 'ProjectDetails',
+            exclude: [],
+          }),
+        },
+      },
+    })
+      projectInfo: ProjectDetails,
+  ): object {
+    // Reply with a greeting, the current time, the url, and request headers
+    return new Promise<any>((resolve, reject) => {
+      MongoDatasource.getClient().then(client => {
+        resolve(this.repository.updateProjectInfo(client, 'Project details received successfully.', projectInfo._id, projectInfo));
+      });
+    });
+  }
+
+  @post('/v1/project/create')
+  createProject(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(ProjectDetails, {
+            title: 'ProjectDetails',
+            exclude: [],
+          }),
+        },
+      },
+    })
+      projectInfo: ProjectDetails,
+  ): object {
+    // Reply with a greeting, the current time, the url, and request headers
+    return new Promise<any>((resolve, reject) => {
+      MongoDatasource.getClient().then(client => {
+        resolve(this.repository.createNewProject(client, 'Project created successfully.', projectInfo));
+      });
+    });
+  }
+}
--- src/repositories/project.repository.ts
+++ src/repositories/project.repository.ts
@@ -38,6 +38,38 @@ export class ProjectRepository {
     }
   }
 
+  async getAllMaterialDetails(client: MongoClient, messageOnSuccess: string): Promise<any> {
+    const collection = client.db('Main').collection('MaterialList');
+    const cursor = collection.find({});
+    const result = await cursor.toArray();
+    if (result !== undefined && result.length > 0) {
+      return {
+        status: 'Success',
+        statusCode: 200,
+        message: messageOnSuccess,
+        data: result,
+      };
+    }
+  }
+
+  async getMaterialInformation(client: MongoClient, messageOnSuccess: string, id: String): Promise<any> {
+    const collection = client.db('Main').collection('MaterialList');
+    const filter = {
+      '_id': id as unknown as ObjectId,
+    };
+
+    const cursor = collection.find(filter);//{projection: {_id: 0}}
+    const result = await cursor.toArray();
+    if (result !== undefined && result.length > 0) {
+      return {
+        status: 'Success',
+        statusCode: 200,
+        message: messageOnSuccess,
+        data: result,
+      };
+    }
+  }
+
   async updateProjectInfo(client: MongoClient, messageOnSuccess: string, id: string, toBeUpdated: any): Promise<any> {
     const collection = client.db('Main').collection('ConstructionProject');
     const filter = {
